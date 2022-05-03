b968ac35448a7b19d516133673521664089c63cb
Angrymouse <rykov.nikas@gmail.com>
2022-05-03 15:57:08 +0300

    release

---
+++ README.md
@@ -0,0 +1,21 @@
+# SifReCompound
+SifReCompound is restake bot for sif staking.
+## To run bot:
+1. Install Node.JS 14+
+2. Configure `recompound.json` by the example given here.
+3. Create file `mnemonic.txt` and put mnemonic from your wallet here. Bot will restake rewards from this wallet.
+4. Run `npm install`
+5. Run `node index.js &`
+6. Enjoy your restake, delegate to Another.Software :)
+
+> Note: This bot is created for sif, where AuthZ isn't implemented yet. When it will be implemented, it's better to move to <https://restake.app>
+
+## Support developer
+
+You can delegate to us [on Ping.pub](https://ping.pub/sifchain/staking/sifvaloper1sz4vscgjuqqntt2pjs65u96mhy5tu5qw958nxt)
+Also you can donate some $ROWAN to us: `sif1sz4vscgjuqqntt2pjs65u96mhy5tu5qwvk0exm`
+
+## License
+This product is released under AnotherSoftwareAttributionLicense ( Modified MIT license ).
+In short: If you modify this product, you must leave "Stake now with Another.Software!" in MEMO of transactions that bot sends. Also if you make UI or any other modification, you must mention Another.Software as original developer.
+
--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const lcdEndpoint = "https://api.sifchain.finance";
 const rpcEndpoint = "https://rpc.sifchain.finance";
 const { request } = require("undici");
 const fs = require("fs");
-const { DirectSecp256k1HdWallet } = require("@cosmjs/proto-signing");
+const {
+	DirectSecp256k1HdWallet,
+	EncodeObject,
+	Registry,
+} = require("@cosmjs/proto-signing");
 const { stringToPath } = require("@cosmjs/crypto");
-const { SigningStargateClient, StargateClient } = require("@cosmjs/stargate");
+const {
+	SigningStargateClient,
+	StargateClient,
+	defaultRegistryTypes,
+} = require("@cosmjs/stargate");
+let recompound = JSON.parse(fs.readFileSync("./recompound.json"));
+const registry = new Registry([...defaultRegistryTypes]);
 (async () => {
 	const mnemonic =
 		fs.readFileSync("./mnemonic.txt", "utf8").split("\n")[0] || "";
@@ -15,15 +25,63 @@ const { SigningStargateClient, StargateClient } = require("@cosmjs/stargate");
 
 	const client = await SigningStargateClient.connectWithSigner(
 		rpcEndpoint,
-		wallet
+		wallet,
+		{ registry: registry }
 	);
+	const fee = {
+		amount: [
+			{
+				denom: "rowan", // Use the appropriate fee denom for your chain
+				amount: "10000000000000",
+			},
+		],
+		gas: "280000",
+	};
+	console.log("Coded with <3 by @angrymouse_hns, founder of Another.Software");
+	recompound.forEach((comp) => {
+		setInterval(async () => {
+			let rewards = await getRewardsFor(firstAccount.address, comp.validator);
 
-	let rewards = await getRewardsFor(
-		firstAccount.address,
-		"sifvaloper1sz4vscgjuqqntt2pjs65u96mhy5tu5qw958nxt"
-	);
+			if (uRowanToRowan(rewards) >= comp.minAmount) {
+				let tx = await client.signAndBroadcast(
+					firstAccount.address,
+					[
+						{
+							typeUrl:
+								"/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward",
 
-	console.log(uRowanToRowan(rewards));
+							value: {
+								delegatorAddress: firstAccount.address,
+								validatorAddress: comp.validator,
+							},
+						},
+						{
+							typeUrl: "/cosmos.staking.v1beta1.MsgDelegate",
+
+							amount: {
+								denom: "rowan",
+								amount: rewards.toString(),
+							},
+							value: {
+								delegatorAddress: firstAccount.address,
+								validatorAddress: comp.validator,
+								amount: {
+									denom: "rowan",
+									amount: rewards.toString(),
+								},
+							},
+						},
+					],
+					fee,
+					"SifReCompound rewards recompound. Stake now with Another.Software!\n https://another.software/sifrecompound"
+				);
+
+				console.log("Successfully restaked! TX id:" + tx.transactionHash);
+			}
+		}, comp.interval);
+	});
+
+	// console.log(uRowanToRowan(rewards));s
 	// console.log(uRowanToRowan(urowan));
 })();
 
